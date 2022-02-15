10d45a1c252b03ec397621a588a5b35f5ecdc548
colderinit <66754842+colderinit@users.noreply.github.com>
2022-02-15 15:36:04 -0600

    Added multiple Ultrasonic prox collection

--- walle/walle.ino
+++ walle/walle.ino
@@ -5,27 +5,35 @@
 #include "NewPing.h"
 
 
-#define DHTPIN_W 5
-#define trigPin_W 6
-#define echoPin_W 7
-#define DHTPIN_E 8
-#define trigPin_E 9
-#define echoPin_E 10
-#define DHTPIN 11
-#define triggerPin 12 
-#define echoPin 13 
-#define MAX_DISTANCE 400 
+#define DHTPIN 2
+// West
+#define trigPin_W 8
+#define echoPin_W 9
+// East
+#define trigPin_E 10
+#define echoPin_E 11
+// North (all in terms of driving forward)
+#define triggerPin 12
+#define echoPin 13
+
+#define MAX_DISTANCE 400
 
 
 #define DHTYPE DHT11
 
 
 NewPing sonar(triggerPin, echoPin, MAX_DISTANCE);
+NewPing sonarE(trigPin_E, echoPin_E, MAX_DISTANCE);
+NewPing sonarW(trigPin_W, echoPin_W, MAX_DISTANCE);
 
 float duration;
+float durationE;
+float durationW;
 float hum;
 float temp;
 float distance;
+float distanceE;
+float distanceW;
 float soundspeedms;
 float soundspeedcm;
 
@@ -33,52 +41,78 @@ int iterations = 6;
 
 DHT dht(DHTPIN, DHTYPE);
 
-void setup()
-{
-    // put your setup code here, to run once:
-    Serial.begin(9600);
-    dht.begin();
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
 }
 
-void loop()
-{
-    delay(2000);
+void loop() {
 
-    hum = dht.readHumidity();
-    temp = dht.readTemperature();
 
-    soundspeedms = 331.4 + (0.606 *temp) +(0.0124 * hum);
+  delay(1000);
 
-    soundspeedcm = soundspeedms / 10000;
-    
-    duration = sonar.ping_median(iterations);
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
 
-    distance = (duration/2)*soundspeedcm;
+  soundspeedms = 331.4 + (0.606 * temp) + (0.0124 * hum);
 
-    Serial.print("Sound :");
-    Serial.print(soundspeedms);
-    Serial.print(" m/s, ");
-    Serial.print("Humidity:");
-    Serial.print(hum);
-    Serial.print("%, Temp:");
-    Serial.print(hum);
-    Serial.print(" C | Distance:");
-    
-    
-    
- 
-    Serial.print("Distance is equal to ");
-    if (distance >= 400 || distance <= 2)
-    {
-        Serial.println("Out of range :(");
-    }
+  soundspeedcm = soundspeedms / 10000;
 
-    else
-    {
-        
-        Serial.print(distance);
-        Serial.println(" cm");
-        delay(500);
-    }
+  // end of temperature sensor data collection
+
+  duration = sonar.ping_median(iterations);
+  durationE = sonarE.ping_median(iterations);
+  durationW = sonarW.ping_median(iterations);
+
+  distance = (duration / 2) * soundspeedcm;
+  distanceE = (durationE / 2) * soundspeedcm;
+  distanceW = (durationW / 2) * soundspeedcm;
+
+  Serial.print("Sound :");
+  Serial.print(soundspeedms);
+  Serial.print(" m/s, ");
+  Serial.print("Humidity:");
+  Serial.print(hum);
+  Serial.print("%, Temp:");
+  Serial.print(temp);
+  Serial.println(" C | Distance:");
+
+
+
+
+  Serial.print("DistanceN is equal to ");
+  if (distance >= 400 || distance <= 2) {
+    Serial.println("Out of range :(");
+  }
+
+  else {
+
+    Serial.print(distance);
+    Serial.println(" cm");
     delay(500);
+  }
+  Serial.print("DistanceE is equal to ");
+  if (distanceE >= 400 || distanceE <= 2) {
+    Serial.println("Out of range :(");
+  }
+
+  else {
+
+    Serial.print(distanceE);
+    Serial.println(" cm");
+    delay(500);
+  }
+  Serial.print("DistanceW is equal to ");
+  if (distanceW >= 400 || distanceW <= 2) {
+    Serial.println("Out of range :(");
+  }
+
+  else {
+
+    Serial.print(distanceW);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
 }
